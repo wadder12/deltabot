e7cc9ac35d933746cd198c91bb240e8b574db5a1
Restyled.io <commits@restyled.io>
2024-03-05 02:38:56 +0000

    Restyled by black

--- bot.py
+++ bot.py
@@ -4,18 +4,20 @@ from nextcord import Intents
 from settings.cogs import load_cogs_async
 from custom.startup.bot_startup import custom_startup
 from dotenv import load_dotenv
+
 load_dotenv()
 
-discord_token = os.getenv('DISCORD_TOKEN')
-command_prefix = os.getenv('COMMAND_PREFIX')
+discord_token = os.getenv("DISCORD_TOKEN")
+command_prefix = os.getenv("COMMAND_PREFIX")
 intents = Intents.all()
 bot = commands.Bot(command_prefix=command_prefix, intents=intents)
 
 
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name}')
+    print(f"Logged in as {bot.user.name}")
     await load_cogs_async(bot)
+
 
 if __name__ == "__main__":
     custom_startup()
